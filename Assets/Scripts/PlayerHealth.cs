95aa19e85dc5761aae26924f05b584f75630b525
Bumzin Yoo <bluerain894@gmail.com>
2021-10-30 01:09:24 +0900

    textmeshpro 사용 hp text 구현

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI; // UI 관련 코드
+using TMPro;
 
 // 플레이어 캐릭터의 생명체로서의 동작을 담당
 public class PlayerHealth : LifeEntity
 {
     public Slider healthSlider; // 체력을 표시할 UI 슬라이더
-
+    public TextMeshProUGUI healthText;
+   
     public AudioClip deathClip; // 사망 소리
     public AudioClip hitClip; // 피격 소리
     public AudioClip itemPickupClip; // 아이템 습득 소리
@@ -15,6 +17,7 @@ public class PlayerHealth : LifeEntity
 
     private PlayerMovement playerMovement; // 플레이어 움직임 컴포넌트
     private PlayerShooter playerShooter; // 플레이어 슈터 컴포넌트
+    private Collider playerCollider;
 
     private void Awake()
     {
@@ -23,6 +26,7 @@ public class PlayerHealth : LifeEntity
         playerAudioPlayer = GetComponent<AudioSource>();
         playerMovement = GetComponent<PlayerMovement>();
         playerShooter = GetComponent<PlayerShooter>();
+        playerCollider = GetComponent<CapsuleCollider>();
 
     }
 
@@ -33,7 +37,6 @@ public class PlayerHealth : LifeEntity
         healthSlider.gameObject.SetActive(true); // 체력 슬라이더 활성화
         healthSlider.maxValue = startingHealth; // 체력 초기화(100)
         healthSlider.value = health; // 체력 슬라이더 값을 현재 체력값으로 변경
-
         playerMovement.enabled = true; // 플레이어 조작을 받는 컴포넌트 활성화
         playerShooter.enabled = true;
     }
@@ -44,6 +47,7 @@ public class PlayerHealth : LifeEntity
         // LivingEntity의 RestoreHealth() 실행 (체력 증가)
         base.RestoreHealth(newHealth);
         healthSlider.value = health; // 갱신된 체력으로 체력 슬라이더 갱신
+        healthText.text = health + "  /  " + startingHealth;
     }
 
     // 데미지 처리
@@ -51,7 +55,13 @@ public class PlayerHealth : LifeEntity
     {
         // LivingEntity의 OnDamage() 실행(데미지 적용)
         base.OnDamage(damage, hitPoint, hitDirection);
+        playerAudioPlayer.PlayOneShot(hitClip);
         healthSlider.value = health;
+        if(health <= 0 )
+        {
+            health = 0;
+        }
+        healthText.text = Mathf.Floor(health) + "  /  " + startingHealth; 
     }
 
     // 사망 처리
@@ -59,11 +69,11 @@ public class PlayerHealth : LifeEntity
     {
         // LivingEntity의 Die() 실행(사망 적용)
         base.Die();
-        healthSlider.gameObject.SetActive(false); // 체력 슬라이더 비활성화
         //playerAudioPlayer.PlayOneShot(deathClip); // 사망음 재생
         playerAnimator.SetTrigger("Die"); // 사망 애니메이션 재생
         playerMovement.enabled = false; // 플레이어 조작을 받는 컴포넌트 비활성화
         playerShooter.enabled = false;
+        playerCollider.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
